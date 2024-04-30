5c7935d17ff6489eb83eeb1511b9197701b43e61
HirumiN <hilminurullah3@gmail.com>
2024-04-30 19:39:58 +0700

    sambung ke welcome page

--- tesLaravel/resources/views/mypage.blade.php
+++ tesLaravel/resources/views/mypage.blade.php
@@ -7,14 +7,8 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="/">
     @vite('resources/css/app.css')
-
-    <!-- Styles -->
-    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 
 <body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover"
@@ -34,16 +28,16 @@
                     style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
 
                 <h1 class="text-3xl font-bold pt-8 lg:pt-0">Muhammad Hilmi Nurullah</h1>
-                <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+                <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-amber-500 opacity-25"></div>
                 <p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-                    <svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+                    <svg class="h-4 fill-current text-amber-700 pr-4" xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20">
                         <path
                             d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
                     </svg> Mahasiswa kupu-kupu
                 </p>
                 <p class="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                    <svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
+                    <svg class="h-4 fill-current text-amber-700 pr-4" xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 20 20">
                         <path
                             d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
@@ -52,14 +46,15 @@
                 <p class="pt-8 text-sm">Remaja yang suka mencari sesuatu yang tidak penting</p>
 
                 <div class="pt-12 pb-8">
-                    <button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-                        Get In Touch
+                    <button onclick="window.location.href='/welcome'" class="bg-amber-700 hover:bg-amber-900 text-white font-bold py-2 px-4 rounded-full">
+                        Welcome Page
                     </button>
                 </div>
+                
 
                 <div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
-                    <a class="link" href="#" data-tippy-content="@facebook_handle">
-                        <svg class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+                    <a class="link" href="https://www.facebook.com/profile.php?id=100010596378481" data-tippy-content="@facebook_handle">
+                        <svg class="h-6 fill-current text-gray-600 hover:text-amber-700" role="img"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Facebook</title>
                             <path
@@ -67,15 +62,15 @@
                         </svg>
                     </a>
                     <a class="link" href="#" data-tippy-content="@twitter_handle">
-                        <svg class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+                        <svg class="h-6 fill-current text-gray-600 hover:text-amber-700" role="img"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Twitter</title>
                             <path
                                 d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
                         </svg>
                     </a>
-                    <a class="link" href="#" data-tippy-content="@github_handle">
-                        <svg class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+                    <a class="link" href="https://github.com/HirumiN" data-tippy-content="@github_handle">
+                        <svg class="h-6 fill-current text-gray-600 hover:text-amber-700" role="img"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>GitHub</title>
                             <path
@@ -83,14 +78,14 @@
                         </svg>
                     </a>
                     <a class="link" href="#" data-tippy-content="@unsplash_handle">
-                        <svg class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+                        <svg class="h-6 fill-current text-gray-600 hover:text-amber-700" role="img"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Unsplash</title>
                             <path d="M7.5 6.75V0h9v6.75h-9zm9 3.75H24V24H0V10.5h7.5v6.75h9V10.5z" />
                         </svg>
                     </a>
                     <a class="link" href="#" data-tippy-content="@instagram_handle">
-                        <svg class="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
+                        <svg class="h-6 fill-current text-gray-600 hover:text-amber-700" role="img"
                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <title>Instagram</title>
                             <path
@@ -98,8 +93,6 @@
                         </svg>
                     </a>
                 </div>
-
-                <!-- Use https://simpleicons.org/ to find the svg for your preferred product -->
 
             </div>
 
